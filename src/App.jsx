0b2ad8a2f050b3979d44b6360216ba8339337498
vanshrajput01 <rajputvansh009@gmail.com>
2023-11-18 20:58:44 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
+import { useState } from 'react'
 import './App.css'
 function App() {
+  let [data,setdata] = useState(null)
+  let [data1,setdata1] = useState(null)
+  let[Result,setresult] = useState(false)
+
+  let getdata = (event) =>{
+    setdata(event.target.value)
+    setresult(false)
+
+  }
+
+  let getdata1 = (event) =>{
+    setdata1(event.target.value)
+    setresult(false)
+
+  }
+  
+  
+
 
   return (
     <div className='main-container'>
       <h1>Form with State React Project</h1>
-      <form className='Form'>
+      <div className='Form'>
         <label>
           Name:
-          <input type="text" name="name" />
+          <input type="text"  name="name" onChange={getdata}/>
         </label>
         <br></br>
         <label>
           Email:
-          <input type="email" name="email" />
+          <input type="email" name="email" onChange={getdata1} />
         </label>
         <br></br>
-        <input type="submit" value="Submit" />
-        <input type="reset" value="Reset" />
-      </form>
+        <button onClick = {() => setresult(true)} type="submit">Submit</button>
+        
+        <h1>Result</h1>
+        {Result ? <p> {data} </p> : null} 
+        {Result ? <p> {data1} </p> : null}       
 
 
+    </div>
+      
     </div>
   )
 }
