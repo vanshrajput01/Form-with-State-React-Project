17a8de453a8ab492096d2f21c84a8e6ac2d8b7e2
vanshrajput01 <rajputvansh009@gmail.com>
2023-11-18 16:50:09 +0530

    Second Commit "App Upadation"

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,20 @@ function App() {
 
   return (
     <div>
+      <h1>Form with State React Project</h1>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <input type="submit" value="Submit" />
+        <input type="reset" value="Reset" />
+      </form>
+
 
     </div>
   )
