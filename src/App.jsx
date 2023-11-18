4e55f82bdbfcea16e4d289d3459b50f7b94ec1b4
vanshrajput01 <rajputvansh009@gmail.com>
2023-11-18 17:18:50 +0530

    Thrid Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import './App.css'
 function App() {
 
   return (
-    <div>
+    <div className='main-container'>
       <h1>Form with State React Project</h1>
-      <form>
+      <form className='Form'>
         <label>
           Name:
           <input type="text" name="name" />
         </label>
+        <br></br>
         <label>
           Email:
           <input type="email" name="email" />
         </label>
+        <br></br>
         <input type="submit" value="Submit" />
         <input type="reset" value="Reset" />
       </form>
